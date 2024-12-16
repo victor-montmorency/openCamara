15ec03c29244fe7bb54301c50d460b6f2cf63fc6
victor-montmorency <67494565+victor-montmorency@users.noreply.github.com>
2024-12-16 18:14:14 -0300

    Initial Commit

---
+++ openData Camara federal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  openData Camara federal
+//
+//  Created by victor mont-morency on 16/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
